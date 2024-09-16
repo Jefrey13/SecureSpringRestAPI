a928d18d27c5d0ee582587f15f28bd3ecfa74ab1
Jefrey13 <jeff1zuniga3@gmail.com>
2024-09-16 17:39:47 -0600

    Configurando Jwt y RefreshToken

---
+++ src/main/java/com/Spring/SecureSpringRestAPI/security/config/JwtTokenUtil.java
@@ -0,0 +1,44 @@
+package com.Spring.SecureSpringRestAPI.security.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenUtil {
+    private final String SECRET_KEY = "my_super_secret_key";
+    private final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 30; // 30 minutos
+    private final long REFRESH_TOKEN_EXPIRATION = 1000 * 60 * 60 * 24; // 24 horas
+
+    public String generateAccessToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public Boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getExpiration().before(new Date());
+    }
+}
